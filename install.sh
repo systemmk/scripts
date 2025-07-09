7a071efb012c629c8bd2b00ac2422727f3769e3f
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2025-07-09 13:37:24 -0300

    fix: melhorando logs de install

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 RED="\e[31m"
 CYAN="\e[36m"
 WHITE="\e[97m"
+TAB="              "
 
 echo -e "${RED} == STEP 1 == ${CYAN}Clonando aplicação 'frontend' ${WHITE}"
 echo
 git clone https://github.com/systemmk/frontend.git
 cd frontend
+
+echo -e "${CYAN}${TAB}Realizando checkout para develop ${WHITE}"
 git checkout develop
+echo -e "${CYAN}${TAB}Instalando dependencias ${WHITE}"
 npm i
 cd ..
 
@@ -15,7 +19,10 @@ echo -e "${RED} == STEP 2 == ${CYAN}Clonando aplicação 'migrations' ${WHITE}"
 echo
 git clone https://github.com/systemmk/migrations.git
 cd migrations
+
+echo -e "${CYAN}${TAB}Realizando checkout para develop ${WHITE}"
 git checkout develop
+echo -e "${CYAN}${TAB}Instalando dependencias ${WHITE}"
 npm i
 cd ..
 
@@ -28,9 +35,9 @@ echo
 echo -e "${RED} == STEP 4 == ${CYAN}Clonando aplicação 'seed-user' ${WHITE}"
 echo
 git clone https://github.com/systemmk/seed-user.git
+cd seed-user
 
-echo
-echo -e "${RED} == STEP 4 == ${CYAN}Executando script run.sh"
-echo
-cd scripts
-./run.sh
+echo -e "${CYAN}${TAB}Instalando dependencias ${WHITE}"
+npm i
+echo -e "${CYAN}${TAB}Executando seed de usuários ${WHITE}"
+npm start
