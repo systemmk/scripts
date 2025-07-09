bf3b2b7360378cc6870717b368b08bdb4268994e
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2025-07-09 13:16:03 -0300

    fix: realizando checkout para develop em aplicações de código fonte

--- install.sh
+++ install.sh
@@ -5,11 +5,17 @@ WHITE="\e[97m"
 echo -e "${RED} == STEP 1 == ${CYAN}Clonando aplicação 'frontend' ${WHITE}"
 echo
 git clone https://github.com/systemmk/frontend.git
+cd frontend
+git checkout develop
+cd ..
 
 echo
 echo -e "${RED} == STEP 2 == ${CYAN}Clonando aplicação 'migrations' ${WHITE}"
 echo
 git clone https://github.com/systemmk/migrations.git
+cd migrations
+git checkout develop
+cd ..
 
 echo
 echo -e "${RED} == STEP 3 == ${CYAN}Clonando aplicação 'scripts' ${WHITE}"
