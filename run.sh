9f4f90783e95f6147fc0cfc96f3f8f5ed5968d95
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2025-07-09 13:18:33 -0300

    feat: inicializando docker via script

--- run.sh
+++ run.sh
@@ -1,25 +1,32 @@
 RED="\e[31m"
 CYAN="\e[36m"
+
 cd ..
 
-echo -e "${RED} == STEP 1 == ${CYAN}Executando migrations local"
+echo -e "${RED} == STEP 1 == ${CYAN}Inicializando docker"
+echo
+cmd.exe /c "start \"\" \"C:\Program Files\Docker\Docker\Docker Desktop.exe\""
+
+
+echo
+echo -e "${RED} == STEP 2 == ${CYAN}Executando migrations local"
 echo
 cd migrations
 npx supabase start
 
 echo
-echo -e "${RED} == STEP 2 == ${CYAN}Abrindo projeto Next.js no vscode"
+echo -e "${RED} == STEP 3 == ${CYAN}Abrindo projeto Next.js no vscode"
 echo
 cd ../frontend
 code .
 
 echo
-echo -e "${RED} == STEP 3 == ${CYAN}Abrindo aplicacao Next.js e Supabase Studio no browser"
+echo -e "${RED} == STEP 4 == ${CYAN}Abrindo aplicacao Next.js e Supabase Studio no browser"
 echo
 start http://127.0.0.1:54323
 start http://127.0.0.1:3000
 
 echo
-echo -e "${RED} == STEP 4 == ${CYAN}Executando projeto Next.js"
+echo -e "${RED} == STEP 5 == ${CYAN}Executando projeto Next.js"
 echo
 npm run dev
